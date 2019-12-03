8f9fbd2689beb0f941fa3718e86cf95ef3b844d4
chrisbenz <cpbenz@protonmail.com>
2019-12-02 22:36:54 -0700

    component organization, new components

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+
+class About extends React.Component {
+  render() {
+      return (
+          <div>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam lacinia rutrum tortor a dictum. Aliquam ut ultrices est. Proin nunc orci, rutrum eu metus vel, rutrum sollicitudin nisl. Nam ornare turpis et erat sagittis, ac vestibulum nibh luctus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Phasellus metus nulla, sollicitudin rhoncus iaculis eget, euismod non lacus. Sed sodales condimentum sodales. Suspendisse rutrum accumsan facilisis. Duis dignissim eros non dui molestie, porta imperdiet sem finibus. Quisque sit amet elit vitae ex lacinia condimentum quis quis eros. In eu posuere magna. 
+          </div>
+      );
+  }
+}
+
+export default About;
