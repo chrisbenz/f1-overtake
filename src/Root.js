825655abccef2daa43663cc6bf1edf269ef3af3b
Chris Benz <chrisbenz922@gmail.com>
2019-12-12 11:30:49 -0700

    New header and root component

---
+++ src/Root.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/Header";
+
+function Root() {
+
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+
+}
+
+export default Root
