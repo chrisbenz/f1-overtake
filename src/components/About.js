ed55f5d10dd558147d73d74ef0b6bcd8ddfe25d8
chrisbenz <cpbenz@protonmail.com>
2019-12-12 14:45:38 -0700

    Routing fixed for home, data, and about

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,45 @@
-import React from "react"
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    backgroundColor: 'black'
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+  toolbar: theme.mixins.toolbar,
+}));
 
 export default function About() {
-    return <h1>About</h1>
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Typography paragraph>
+          Dif
+        </Typography>
+      </main>
+    </div>
+  );
 }
