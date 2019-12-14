8734043a9317e5b79a20b75101d60dcdf250ce6c
chrisbenz <cpbenz@protonmail.com>
2019-12-14 00:53:10 -0700

    Fetched constructor and driver data

--- src/components/data/DataContainer.js
+++ src/components/data/DataContainer.js
@@ -7,6 +7,7 @@ export default class DataContainer extends React.Component {
     super()
     this.state = {
       raceInfo: [],
+      drivers: [],
       isLoading: true
     }
   }
@@ -16,6 +17,18 @@ export default class DataContainer extends React.Component {
       .then(response => response.json())
       .then(data => this.setState({ 
         raceInfo: data.MRData.RaceTable.Races,
+      }));
+
+    fetch("http://ergast.com/api/f1/2019/drivers.json")
+      .then(response => response.json())
+      .then(data => this.setState({ 
+        drivers: data.MRData.DriverTable.Drivers,
+      }));
+
+    fetch("http://ergast.com/api/f1/2019/constructors.json")
+      .then(response => response.json())
+      .then(data => this.setState({ 
+        drivers: data.MRData.ConstructorTable.Constructors,
         isLoading: false,
       }));
   }
